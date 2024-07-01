8b25ccd3a66ead3784b2f251b9441b0bbe6b3b3e
Viraj <150922897+viraj-ap@users.noreply.github.com>
2024-07-01 12:56:51 +0530

    Add files via upload

---
+++ passNinja/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Manager />
+      <Footer />
+
+
+    </>
+  )
+}
+
+export default App
